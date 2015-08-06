1766a02763cbc483980cefc8f11451a05cbb7bb8
mwitlok <mwitlok@gmail.com>
2015-08-06 11:01:59 +0200

    Added routing for areas

--- ImdbWeb/App_Start/RouteConfig.cs
+++ ImdbWeb/App_Start/RouteConfig.cs
@@ -7,19 +7,20 @@ using System.Web.Routing;
 
 namespace ImdbWeb
 {
-    public class RouteConfig
-    {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+	public class RouteConfig
+	{
+		public static void RegisterRoutes(RouteCollection routes)
+		{
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
 			routes.MapMvcAttributeRoutes();
 
 			routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
-        }
-    }
+				name: "Default",
+				url: "{controller}/{action}/{id}",
+				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+				namespaces: new[] { "ImdbWeb.Controllers" }
+			);
+		}
+	}
 }
